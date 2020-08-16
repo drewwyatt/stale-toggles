c807456a59888303021711e1d200b709369e36a5
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-08-15 22:43:05 -0400

    remove secrets debugging

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
-import { debug, setOutput, setFailed } from '@actions/core'
+import { setOutput, setFailed } from '@actions/core'
 import * as inputs from './inputs'
 import { getFeatures } from './optimizely'
 
-const listInputs = () => {
-  for (const key of Object.keys(inputs)) {
-    debug(`[input][${key}] ${inputs[key as keyof typeof inputs]}`)
-  }
-}
-
 const run = async () => {
   try {
-    listInputs()
     const features = await getFeatures(inputs.PROJECT, inputs.TOKEN)
     setOutput('features', features)
   } catch (error) {
